6d9784b00c4533750af1db8ac790a33af2c2be69
Greg <51519968+lordfather425@users.noreply.github.com>
2023-03-04 22:10:28 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,15 +1,5 @@
-// Listen for a click on the browser action icon
+// Listen for the extension button to be clicked
 chrome.browserAction.onClicked.addListener(function(tab) {
-  // Send a message to the content script to extract the year, make, and model from the Facebook Marketplace listing
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {action: "extract_data"}, function(response) {
-      // Send a message to the content script to search CarGurus.com for the extracted data
-      chrome.tabs.sendMessage(tabs[0].id, {action: "search_cargurus", data: response}, function(response) {
-        // Display the average listing price in a pop-up window
-        chrome.tabs.create({url: chrome.runtime.getURL('popup.html'), active: true}, function(tab) {
-          chrome.runtime.sendMessage({action: "display_price", data: response});
-        });
-      });
-    });
-  });
+  // Send a message to the content script to initiate the search
+  chrome.tabs.sendMessage(tab.id, {action: "search"});
 });
