e3cb69e57cf6d28d36fd522d8d9e35b44a15b9a9
Greg <51519968+lordfather425@users.noreply.github.com>
2023-03-04 22:29:08 -0500

    Update content.js

--- content.js
+++ content.js
@@ -1,25 +1,30 @@
-// Listen for messages from the background script
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  // If the message is to initiate the search
   if (request.action === "search") {
-    // Extract the year, make, and model of the vehicle listing on Facebook Marketplace
-    const title = document.querySelector('h1 span').innerText;
-    const [year, make, model] = title.split(' ');
+    const vehicleInfo = {
+      year: "",
+      make: "",
+      model: ""
+    };
 
-    // Construct the search URL for CarGurus.com
-    const searchUrl = `https://www.cargurus.com/Cars/inventorylisting/ajaxFetchSubsetInventoryListing.action?sourceContext=untrackedExternal_generic&newSearchFromOverviewPage=true&inventorySearchWidgetType=AUTO&entitySelectingHelper.selectedEntity=d675&entitySelectingHelper.selectedEntity2=&zip=21044&distance=50&searchChanged=true&modelChanged=false&filtersModified=true&sortType=PRICE&sortDirection=ASC&pageNumber=1&minPrice=0&maxPrice=30000&minMileage=0&maxMileage=200000&selectedTransmissions=&selectedTrimList=${model}&selectedYear=${year}&selectedMakeName=${make}&originSiteId=&analyticsTrackingGroup=&_pageName=HomePage&action=inFrame`;
-    
-    // Make an HTTP request to CarGurus.com
-    fetch(searchUrl)
-      .then(response => response.json())
-      .then(data => {
-        // Extract the average listing price from the search results
-        const { listings } = data;
-        const prices = listings.map(listing => listing.price);
-        const averagePrice = prices.reduce((acc, curr) => acc + curr) / prices.length;
+    // get year, make, and model from Facebook Marketplace listing
+    const listingTitle = document.querySelector("h1._3yjI").textContent;
+    const vehicleInfoMatches = listingTitle.match(/(\d{4})\s+(.*)\s+(.*)/);
+    if (vehicleInfoMatches) {
+      vehicleInfo.year = vehicleInfoMatches[1];
+      vehicleInfo.make = vehicleInfoMatches[2];
+      vehicleInfo.model = vehicleInfoMatches[3];
+    }
 
-        // Display the average listing price in a pop-up window
-        alert(`The average listing price for a ${year} ${make} ${model} on CarGurus.com is $${averagePrice.toFixed(2)}`);
-      });
+    // send year, make, and model to background script to get average price from CarGurus
+    chrome.runtime.sendMessage({ action: "getAveragePrice", data: vehicleInfo }, function(response) {
+      if (response.error) {
+        console.error(response.error);
+      } else {
+        console.log(response.averagePrice); // for debugging
+        sendResponse({ averagePrice: response.averagePrice });
+      }
+    });
+
+    return true; // needed to make the message passing async
   }
 });
