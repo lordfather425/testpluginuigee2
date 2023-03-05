83ca0d53f388379fbf336798a71e094f941733c4
Greg <51519968+lordfather425@users.noreply.github.com>
2023-03-04 22:28:49 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,5 +1,23 @@
-// Listen for the extension button to be clicked
 chrome.browserAction.onClicked.addListener(function(tab) {
-  // Send a message to the content script to initiate the search
   chrome.tabs.sendMessage(tab.id, {action: "search"});
+});
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.action === "getAveragePrice") {
+    const { year, make, model } = request.data;
+    const url = `https://www.cargurus.com/Cars/instantMarketValueFromVIN.action?startUrl=%2F&carDescription.year=${year}&carDescription.makeName=${make}&carDescription.modelName=${model}&mileage=0&engine=0&transmission=0&driveWheelConfiguration=0&options=0&selectedEntity=d2525`;
+
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        const averagePrice = data.averagePrice;
+        sendResponse({ averagePrice });
+      })
+      .catch(error => {
+        console.error(error);
+        sendResponse({ error });
+      });
+
+    return true; // needed to make the message passing async
+  }
 });
