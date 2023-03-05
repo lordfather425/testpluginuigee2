3dfc595a84228b46f8da11d852d7272e660cd6f0
Greg <51519968+lordfather425@users.noreply.github.com>
2023-03-04 22:20:30 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,26 +1,5 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.action == "search") {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      var tab = tabs[0];
-      var url = tab.url;
-      // Extract year, make, and model from Facebook Marketplace
-      // Use extracted data to search CarGurus.com and extract average price
-      // Send result back to content script
-      sendResponse({price: averagePrice});
-    });
-  }
-  return true;
-});
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.action == "search") {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      var tab = tabs[0];
-      var url = tab.url;
-      // Extract year, make, and model from Facebook Marketplace
-      // Use extracted data to search CarGurus.com and extract average price
-      // Send result back to content script
-      sendResponse({price: averagePrice});
-    });
-  }
-  return true;
+// Listen for the extension button to be clicked
+chrome.browserAction.onClicked.addListener(function(tab) {
+  // Send a message to the content script to initiate the search
+  chrome.tabs.sendMessage(tab.id, {action: "search"});
 });
